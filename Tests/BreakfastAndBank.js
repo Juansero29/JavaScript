62a900b8d3c250d83ad1c3943d76cadce4ba24f9
Juansero29 <juansero29@gmail.com>
2017-05-06 09:51:56 +0200

    - Added bank example

--- Tests/BreakfastAndBank.js
+++ Tests/BreakfastAndBank.js
@@ -6,3 +6,22 @@ var breakfast = {
 
 console.log(breakfast);
 
+var savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+    if (amount > 0) {
+      savingsAccount.balance += amount;
+    }
+  },
+  withdraw: function removeMoney(amount) {
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
+  },
+  printSummary : function printAccountSummary(){
+    console.log("Welcome!\nYour balance is currently " + this.balance + 
+    " and your interest rate is" + this.interestRatePercent + "%.");}
+
+  };
